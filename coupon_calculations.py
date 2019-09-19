db2bf71548f5f7f097fc5c7ff2246b8b805377d1
ry-blank <52143833+ry-blank@users.noreply.github.com>
2019-09-18 20:01:47 -0500

    Add files via upload

---
+++ coupon_calculations.py
@@ -0,0 +1,40 @@
+"""
+Program: Nested if statement - Module 4
+Author: Ryan Blankenship
+Last date modified: 09/18/2019
+The purpose of this program is to
+calculate the price of an item after
+deducting coupons, then adding tax
+and shipping costs.
+"""
+
+
+def calculate_price(price, cash_coupon, percent_coupon):
+    TAX = 0.06
+    TEN_OR_LESS_SHIPPING = 5.95
+    TEN_UP_TO_THIRTY_SHIPPING = 7.95
+    THIRTY_UP_TO_FIFTY = 11.95
+    FIFTY_OR_OVER = 0.00
+
+    total_after_cash_coupon = price - cash_coupon
+    total_after_percent_coupon = total_after_cash_coupon * percent_coupon
+    total_after_percent_coupon = total_after_cash_coupon - total_after_percent_coupon
+    total_to_add_to_tax = total_after_percent_coupon * TAX
+    total_after_tax = total_after_percent_coupon + total_to_add_to_tax
+    # if total_after_tax < 10.00:
+    #     total = total_after_tax + TEN_OR_LESS_SHIPPING
+    # elif total_after_tax >= 10.00 or total_after_tax < 30.00:
+    #     total = total_after_tax + TEN_UP_TO_THIRTY_SHIPPING
+    # elif total_after_tax >= 30.00 or total_after_tax < 50.00:
+    #     total = total_after_tax + THIRTY_UP_TO_FIFTY
+    # else:
+    #     total = total_after_tax + FIFTY_OR_OVER
+    # return round(total, 2)
+
+
+if __name__ == '__main__':
+    price_of_item = 35.00
+    cash_pon = 10.00
+    percent_pon = .10
+    net_total = calculate_price(price_of_item, cash_pon, percent_pon)
+    print(net_total)
---
+++ test_coupon_calculations.py
@@ -0,0 +1,88 @@
+import unittest
+from store import coupon_calculations
+
+
+class MyTestCase(unittest.TestCase):
+    # Test Set1
+
+    def test_ten_or_less_shipping_test1(self):
+        self.assertEqual(coupon_calculations.calculate_price(8.50, 5.00, .10), 9.29)
+
+    def test_ten_or_less_shipping_test2(self):
+        self.assertEqual(coupon_calculations.calculate_price(7.50, 5.00, .15), 8.2)
+
+    def test_ten_or_less_shipping_test3(self):
+        self.assertEqual(coupon_calculations.calculate_price(6.50, 5.00, .20), 7.22)
+
+    def test_ten_or_less_shipping_test4(self):
+        self.assertEqual(coupon_calculations.calculate_price(8.50, 10.00, .10), 4.52)
+
+    def test_ten_or_less_shipping_test5(self):
+        self.assertEqual(coupon_calculations.calculate_price(7.50, 10.00, .15), 3.7)
+
+    def test_ten_or_less_shipping_test6(self):
+        self.assertEqual(coupon_calculations.calculate_price(6.50, 10.00, .20), 2.98)
+
+    # Test Set2
+
+    # def test_ten_up_to_thirty_test1(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(27.50, 5.00, .10), 29.41)
+    #
+    # def test_ten_up_to_thirty_test2(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(26.50, 5.00, .15), 27.32)
+    #
+    # def test_ten_up_to_thirty_test3(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(25.50, 5.00, .20), 25.33)
+    #
+    # def test_ten_up_to_thirty_test4(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(27.50, 10.00, .10), 24.64)
+    #
+    # def test_ten_up_to_thirty_test5(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(26.50, 10.00, .15), 22.82)
+    #
+    # def test_ten_up_to_thirty_test6(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(25.50, 10.00, .20), 21.09)
+    #
+    # # Test Set3
+    #
+    # def test_thirty_up_to_fifty_test1(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(42.50, 5.00, .10), 43.73)
+    #
+    # def test_thirty_up_to_fifty_test2(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(40.50, 5.00, .15), 39.94)
+    #
+    # def test_thirty_up_to_fifty_test3(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(38.50, 5.00, .20), 36.36)
+    #
+    # def test_thirty_up_to_fifty_test4(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(42.50, 10.00, .10), 38.95)
+    #
+    # def test_thirty_up_to_fifty_test5(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(40.50, 10.00, .15), 35.43)
+    #
+    # def test_thirty_up_to_fifty_test6(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(38.50, 10.00, .20), 32.12)
+    #
+    # # Test Set4
+    #
+    # def test_fifty_or_over_test1(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(75.00, 5.00, .10), 74.73)
+    #
+    # def test_fifty_or_over_test2(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(75.00, 5.00, .15), 71.02)
+    #
+    # def test_fifty_or_over_test3(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(75.00, 5.00, .20), 67.31)
+    #
+    # def test_fifty_or_over_test4(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(75.00, 10.00, .10), 69.96)
+    #
+    # def test_fifty_or_over_test5(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(75.00, 10.00, .15), 66.52)
+    #
+    # def test_fifty_or_over_test6(self):
+    #     self.assertEqual(coupon_calculations.calculate_price(75.00, 10.00, .20), 63.07)
+
+
+if __name__ == '__main__':
+    unittest.main()
