424581a7c158a68f999ce0624a939742b20caf1e
ry-blank <52143833+ry-blank@users.noreply.github.com>
2019-09-18 20:05:11 -0500

    Test File
    2nd Commit

--- test_coupon_calculations.py
+++ test_coupon_calculations.py
@@ -25,24 +25,24 @@ class MyTestCase(unittest.TestCase):
 
     # Test Set2
 
-    # def test_ten_up_to_thirty_test1(self):
-    #     self.assertEqual(coupon_calculations.calculate_price(27.50, 5.00, .10), 29.41)
-    #
-    # def test_ten_up_to_thirty_test2(self):
-    #     self.assertEqual(coupon_calculations.calculate_price(26.50, 5.00, .15), 27.32)
-    #
-    # def test_ten_up_to_thirty_test3(self):
-    #     self.assertEqual(coupon_calculations.calculate_price(25.50, 5.00, .20), 25.33)
-    #
-    # def test_ten_up_to_thirty_test4(self):
-    #     self.assertEqual(coupon_calculations.calculate_price(27.50, 10.00, .10), 24.64)
-    #
-    # def test_ten_up_to_thirty_test5(self):
-    #     self.assertEqual(coupon_calculations.calculate_price(26.50, 10.00, .15), 22.82)
-    #
-    # def test_ten_up_to_thirty_test6(self):
-    #     self.assertEqual(coupon_calculations.calculate_price(25.50, 10.00, .20), 21.09)
-    #
+    def test_ten_up_to_thirty_test1(self):
+        self.assertEqual(coupon_calculations.calculate_price(27.50, 5.00, .10), 29.41)
+
+    def test_ten_up_to_thirty_test2(self):
+        self.assertEqual(coupon_calculations.calculate_price(26.50, 5.00, .15), 27.32)
+
+    def test_ten_up_to_thirty_test3(self):
+        self.assertEqual(coupon_calculations.calculate_price(25.50, 5.00, .20), 25.33)
+
+    def test_ten_up_to_thirty_test4(self):
+        self.assertEqual(coupon_calculations.calculate_price(27.50, 10.00, .10), 24.64)
+
+    def test_ten_up_to_thirty_test5(self):
+        self.assertEqual(coupon_calculations.calculate_price(26.50, 10.00, .15), 22.82)
+
+    def test_ten_up_to_thirty_test6(self):
+        self.assertEqual(coupon_calculations.calculate_price(25.50, 10.00, .20), 21.09)
+
     # # Test Set3
     #
     # def test_thirty_up_to_fifty_test1(self):
